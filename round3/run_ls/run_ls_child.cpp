aad53cc005f8819606643dfdebf2a296d795ea8b
pyryl <pyry.laaksonen@hotmail.com>
2023-09-12 11:45:32 +0300

    Round 3, ex 3

---
+++ round3/run_ls/run_ls_child.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(){
+  const char* args[] = {"/bin/ls", NULL};
+  const char* envs[] = {NULL};
+  int status;
+  if(fork() != 0){
+    waitpid(-1, &status, 0);
+  }else{
+    execve("/bin/ls", const_cast<char**>(args), const_cast<char**>(envs));
+  }
+  return EXIT_SUCCESS;
+}
