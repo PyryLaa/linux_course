e3e9ec5b1f227badd71e6e8c6977992847c8b3a9
pyryl <pyry.laaksonen@hotmail.com>
2023-09-12 11:36:25 +0300

    Round 3, ex 2

--- round3/run_ls/run_ls.cpp
+++ round3/run_ls/run_ls.cpp
@@ -7,13 +7,8 @@ int main(){
   const char* args[] = {"/bin/ls", NULL};
   const char* envs[] = {NULL};
 
-  int status;
-
-  if(fork() != 0){
-    waitpid(-1, &status, 0);
-    std::cout << status << std::endl;
-  }else{
-    execve("/bin/ls", const_cast<char**>(args), const_cast<char**>(envs));
-  }
+ 
+  execve("/bin/ls", const_cast<char**>(args), const_cast<char**>(envs));
+  
   return EXIT_SUCCESS;
 }
