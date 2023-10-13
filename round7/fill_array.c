3d7ca759cf96b70750dcd4d5d0564f8d3a199087
pyryl <pyry.laaksonen@hotmail.com>
2023-10-13 22:30:25 +0300

    Ex 4 done

--- round7/fill_array.c
+++ round7/fill_array.c
@@ -7,13 +7,17 @@
 int main(int argc, char *argv[]){
   int *arr = (int*)malloc(MAX_ARR_SIZE * sizeof(int));
 
-  for(int i = 0; i <= MAX_ARR_SIZE; i++){
+  for(int i = 0; i < MAX_ARR_SIZE; i++){
     arr[i] = i;
+      
   }
   if(argc == 1){
-    for(int j = MAX_ARR_SIZE - 100; j <= MAX_ARR_SIZE; j++){
+    for(int j = MAX_ARR_SIZE - 100; j < MAX_ARR_SIZE; j++){
       printf("%d\n", arr[j]);
-    } 
+          
+    }
+      
   }
   return 0;
+  
 }
---
+++ round7/fill_array_thread.c
@@ -0,0 +1,69 @@
+/*Program to fill 100 million integer array
+  with threads*/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define ARR_SIZE 100000000
+#define THREAD_AMOUNT 10
+
+void *fill_array(void *data);
+
+int *pArr;
+
+typedef struct{ //Struct to hold data for the threads
+  int thid;
+  int start;
+  int end;
+} ThreadData;
+
+int main(int argc, char *argv[]){
+  
+  int* arr = (int*)malloc(ARR_SIZE * sizeof(int));
+  pthread_t threads[THREAD_AMOUNT];
+  ThreadData thread_data[THREAD_AMOUNT]; //Array for every thread's data
+  
+  intptr_t status;
+  int thread_elements = ARR_SIZE / THREAD_AMOUNT;
+  
+  pArr = arr;
+
+  for(int i = 0; i < THREAD_AMOUNT; i++){
+    thread_data[i].thid = i;
+    thread_data[i].start = i * thread_elements; //Starting index for thread to fill the array
+    if(i == THREAD_AMOUNT - 1){
+      thread_data[i].end = ARR_SIZE;
+    }else{
+      thread_data[i].end = ((i+1)*thread_elements) - 1;
+    }
+    
+    status = pthread_create(&threads[i], NULL, fill_array, &thread_data[i]);
+    if(status != 0){
+      perror("Thread creation failed: ");
+      printf("%d", errno);
+    }
+  }
+  for(int i = 0; i < THREAD_AMOUNT; i++){
+    pthread_join(threads[i], NULL);
+  }
+  if(argc < 2){
+    for(int i = ARR_SIZE - 100; i < ARR_SIZE; i++){
+      printf("%d\n", arr[i]);
+    }
+  }
+  return 0;
+}
+
+void *fill_array(void* data){
+  ThreadData* thdata = (ThreadData*)data;
+  int start = thdata -> start;
+  int end = thdata -> end;
+
+  for(int i = start; i < end; i++){
+    pArr[i] = i;
+  }
+  pthread_exit(NULL);
+}
